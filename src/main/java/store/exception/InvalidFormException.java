79fcf26b2d569098d3f5483c222bf29a028736a6
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-27 17:46:18 +0900

    feat: 상황에 따라 다른 예외처리 메시지 출력하는 기능 추가

---
+++ src/main/java/store/exception/InvalidFormException.java
@@ -0,0 +1,8 @@
+package store.exception;
+
+
+public class InvalidFormException extends IllegalArgumentException {
+    public InvalidFormException() {
+        super("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+    }
+}
