ee3a4f7e47805857c72d5198fcb9b253c3eeb737
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-29 21:50:36 +0900

    test: 감소시키는 재고 테스트하는 코드 추가

--- src/test/java/store/StoreTest.java
+++ src/test/java/store/StoreTest.java
@@ -3,15 +3,17 @@ package store;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import store.constant.PromotionPeriodState;
-import store.model.Order;
-import store.model.Product;
-import store.model.Products;
-import store.model.Store;
+import store.model.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertNowTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StoreTest {
@@ -25,14 +27,14 @@ public class StoreTest {
         products.add(new Product("오렌지주스,1800,2,null"));
         store.addProduct("오렌지주스", new Products(products));
         products = new ArrayList<>();
-        products.add(new Product("고구마,1800,2,null"));
-        store.addProduct("고구마", new Products(products));
+        products.add(new Product("물,500,7,null"));
+        store.addProduct("물", new Products(products));
     }
 
     @Test
-    @DisplayName("프로모션 기간이 아닌 주문인지 판별할 수 있다.")
-    void shouldDetermineIfNotPromotionPeriod() {
-        Order order = new Order("[고구마-1]", store);
+    @DisplayName("프로모션 기간 아닐 때 - 프로모션 헤택이 없는 주문인지 판별할 수 있다.")
+    void shouldDetermineIfNotPromotionBenefit() {
+        Order order = new Order("[물-1]", store);
         assertThat(store.executeWhenNotPromotionPeriod(order)).isEqualTo(true);
         assertThat(store.executeWhenPromotionPeriod(order)).isEqualTo(PromotionPeriodState.NOTHING);
         assertThat(store.checkGetOneFree(order)).isEqualTo(false);
@@ -40,7 +42,19 @@ public class StoreTest {
     }
 
     @Test
-    @DisplayName("1개 수량을 추가할 수 있는 주문인지 판별할 수 있다.")
+    @DisplayName("프로모션 기간 아닐 때 - 프로모션 혜택이 가능한 상품이지만 기간이 아닌 주문인지 판별할 수 있다.")
+    void shouldDetermineIfHasPromotionBenefitButNotPromotionPeriod() {
+        assertNowTest(() -> {
+            Order order = new Order("[오렌지주스-3]", store);
+            assertThat(store.executeWhenNotPromotionPeriod(order)).isEqualTo(true);
+            assertThat(store.executeWhenPromotionPeriod(order)).isEqualTo(PromotionPeriodState.NOTHING);
+            assertThat(store.checkGetOneFree(order)).isEqualTo(false);
+            assertThat(store.checkBuyOriginalPrice(order)).isEqualTo(false);
+        }, LocalDate.of(1999, 1, 1).atStartOfDay());
+    }
+
+    @Test
+    @DisplayName("프로모션 기간일 때 - 1개 수량을 추가할 수 있는 주문인지 판별할 수 있다.")
     void shouldDetermineIfProductCanBeAdded() {
         Order order = new Order("[오렌지주스-1]", store);
         assertThat(store.executeWhenNotPromotionPeriod(order)).isEqualTo(false);
@@ -50,7 +64,7 @@ public class StoreTest {
     }
 
     @Test
-    @DisplayName("정가로 결제해야 하는 수량이 있는 주문인지 판별할 수 있다.")
+    @DisplayName("프로모션 기간일 때 - 정가로 결제해야 하는 수량이 있는 주문인지 판별할 수 있다.")
     void shouldDetermineIfOrderHasOriginalPrice() {
         Order order = new Order("[오렌지주스-9]", store);
         assertThat(store.executeWhenNotPromotionPeriod(order)).isEqualTo(false);
@@ -60,12 +74,114 @@ public class StoreTest {
     }
 
     @Test
-    @DisplayName("추가로 물어볼 필요가 없는 주문인지 판별할 수 있다.")
+    @DisplayName("프로모션 기간일 때 - 추가로 물어볼 필요가 없는 주문인지 판별할 수 있다.")
     void shouldDetermineNothingToAskProducts() {
         Order order = new Order("[오렌지주스-2]", store);
         assertThat(store.executeWhenNotPromotionPeriod(order)).isEqualTo(false);
         assertThat(store.executeWhenPromotionPeriod(order)).isEqualTo(PromotionPeriodState.NOTHING);
         assertThat(store.checkGetOneFree(order)).isEqualTo(false);
         assertThat(store.checkBuyOriginalPrice(order)).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("프로모션 기간 아닐 때 재고관리 - 일반재고밖에 없을 때 일반재고를 올바르게 감소시킬 수 있다.")
+    void shouldDecreaseNormalWhenOnlyNormal() {
+        Order order = new Order("[물-1]", store);
+        store.executeWhenNotPromotionPeriod(order);
+        Receipt receipt = store.getReceipt(true);
+
+        Products products = store.getProducts(order.getName());
+        Product promotion = products.getPromotionProduct();
+        Product normal = products.getNormalProduct();
+
+        assertThat(receipt.getTotalPurchaseCount()).isEqualTo(1);
+        assertThat(promotion.getQuantity()).isEqualTo(0);
+        assertThat(normal.getQuantity()).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("프로모션 기간 아닐 때 재고관리 - 프로모션 혜택이 가능한 상품이지만 기간이 아닐 때 일반재고를 우선 감소시킬 수 있다.")
+    void shouldDecreaseNormalWhenNotPromotionPeriodAndHasPromotionBenefit() {
+        assertNowTest(() -> {
+            Order order = new Order("[오렌지주스-3]", store);
+            store.executeWhenNotPromotionPeriod(order);
+            Receipt receipt = store.getReceipt(true);
+
+            Products products = store.getProducts(order.getName());
+            Product promotion = products.getPromotionProduct();
+            Product normal = products.getNormalProduct();
+
+            assertThat(receipt.getTotalPurchaseCount()).isEqualTo(3);
+            assertThat(promotion.getQuantity()).isEqualTo(8);
+            assertThat(normal.getQuantity()).isEqualTo(0);
+
+        }, LocalDate.of(1999, 1, 1).atStartOfDay());
+    }
+
+    @Test
+    @DisplayName("프로모션 기간일 때 재고관리 - 1개 수량을 추가 결제할 때 프로모션 재고를 올바르게 감소시킬 수 있다.")
+    void shouldDecreasePromotionWhenPurchaseOneProduct() {
+        Order order = new Order("[오렌지주스-1]", store);
+        store.calculateWhenGetOneFreeCase(order, true);
+        Receipt receipt = store.getReceipt(true);
+
+        Products products = store.getProducts(order.getName());
+        Product promotion = products.getPromotionProduct();
+        Product normal = products.getNormalProduct();
+
+        assertThat(receipt.getTotalPurchaseCount()).isEqualTo(2);
+        assertThat(promotion.getQuantity()).isEqualTo(7);
+        assertThat(normal.getQuantity()).isEqualTo(2);
+    }
+
+    @DisplayName("프로모션 기간일 때 재고관리 - 정가로 결제해야 하는 수량을 결제하지 않을 때 올바르게 재고를 감소시킬 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"[오렌지주스-9],8,1,2", "[오렌지주스-10],8,1,2"})
+    void shouldDecreaseWhenNotPurchaseOriginalPrice(String name, int expectedTotalPurchase, int expectedPromotionQuantity, int expectedNormalPurchase) {
+        Order order = new Order(name, store);
+        store.calculateWhenBuyOriginalPrice(order, false);
+        Receipt receipt = store.getReceipt(true);
+
+        Products products = store.getProducts(order.getName());
+        Product promotion = products.getPromotionProduct();
+        Product normal = products.getNormalProduct();
+
+        assertThat(receipt.getTotalPurchaseCount()).isEqualTo(expectedTotalPurchase);
+        assertThat(promotion.getQuantity()).isEqualTo(expectedPromotionQuantity);
+        assertThat(normal.getQuantity()).isEqualTo(expectedNormalPurchase);
+    }
+
+
+    @DisplayName("프로모션 기간일 때 재고관리 - 정가로 결제해야 하는 수량을 결제할 때 올바르게 재고를 감소시킬 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"[오렌지주스-9],9,0,2", "[오렌지주스-10],10,0,1"})
+    void shouldDecreaseWhenPurchaseOriginalPrice(String name, int expectedTotalPurchase, int expectedPromotionQuantity, int expectedNormalPurchase) {
+        Order order = new Order(name, store);
+        store.calculateWhenBuyOriginalPrice(order, true);
+        Receipt receipt = store.getReceipt(true);
+
+        Products products = store.getProducts(order.getName());
+        Product promotion = products.getPromotionProduct();
+        Product normal = products.getNormalProduct();
+
+        assertThat(receipt.getTotalPurchaseCount()).isEqualTo(expectedTotalPurchase);
+        assertThat(promotion.getQuantity()).isEqualTo(expectedPromotionQuantity);
+        assertThat(normal.getQuantity()).isEqualTo(expectedNormalPurchase);
+    }
+
+    @Test
+    @DisplayName("프로모션 기간일 때 재고관리 - 추가로 물어볼 필요가 없는 주문일 때 재고를 올바르게 감소시킬 수 있다.")
+    void shouldDecreaseWhenNothingToAsk() {
+        Order order = new Order("[오렌지주스-2]", store);
+        store.calculateWhenNothingToAsk(order);
+        Receipt receipt = store.getReceipt(true);
+
+        Products products = store.getProducts(order.getName());
+        Product promotion = products.getPromotionProduct();
+        Product normal = products.getNormalProduct();
+
+        assertThat(receipt.getTotalPurchaseCount()).isEqualTo(2);
+        assertThat(promotion.getQuantity()).isEqualTo(7);
+        assertThat(normal.getQuantity()).isEqualTo(2);
     }
 }
