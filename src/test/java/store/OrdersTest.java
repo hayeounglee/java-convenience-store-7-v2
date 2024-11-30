6b809f0de74eb5a0146da235e8ae5a7f845e16ff
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-11-30 12:32:38 +0900

    test: 증정품의 개수와, 할인은 올바르게 계산 하는지 테스트하는 코드 작성

--- src/test/java/store/OrdersTest.java
+++ src/test/java/store/OrdersTest.java
@@ -3,7 +3,6 @@ package store;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import store.model.Orders;
 import store.model.Product;
@@ -26,7 +25,7 @@ public class OrdersTest {
         products.add(new Product("밤티라미수,4500,2,null"));
         store.addProduct("밤티라미수", new Products(products));
     }
-    
+
     @DisplayName("중복되는 상품 이름을 입력하면 예외가 발생한다.")
     @ParameterizedTest
     @ValueSource(strings = {"[밤티라미수-1],[밤티라미수-1]"})
--- src/test/java/store/StoreTest.java
+++ src/test/java/store/StoreTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import store.constant.PromotionPeriodState;
 import store.model.*;
 
@@ -29,6 +28,10 @@ public class StoreTest {
         products = new ArrayList<>();
         products.add(new Product("물,500,7,null"));
         store.addProduct("물", new Products(products));
+        products = new ArrayList<>();
+        products.add(new Product("컵라면,1700,0,MD추천상품"));
+        products.add(new Product("컵라면,1700,10,null"));
+        store.addProduct("컵라면", new Products(products));
     }
 
     @Test
@@ -84,7 +87,7 @@ public class StoreTest {
     }
 
     @Test
-    @DisplayName("프로모션 기간 아닐 때 재고관리 - 일반재고밖에 없을 때 일반재고를 올바르게 감소시킬 수 있다.")
+    @DisplayName("프로모션 기간 아닐 때 재고관리 - 일반재고밖에 없을 때 일반재고를 올바르게 감소시킬 수 있다. 또한 증정품의 개수와, 멤버십 할인을 올바르게 계산한다.")
     void shouldDecreaseNormalWhenOnlyNormal() {
         Order order = new Order("[물-1]", store);
         store.executeWhenNotPromotionPeriod(order);
@@ -97,10 +100,13 @@ public class StoreTest {
         assertThat(receipt.getTotalPurchaseCount()).isEqualTo(1);
         assertThat(promotion.getQuantity()).isEqualTo(0);
         assertThat(normal.getQuantity()).isEqualTo(6);
+
+        assertThat(receipt.getGiftProducts()).size().isEqualTo(0);
+        assertThat(receipt.getMembershipDiscount()).isEqualTo(150);
     }
 
     @Test
-    @DisplayName("프로모션 기간 아닐 때 재고관리 - 프로모션 혜택이 가능한 상품이지만 기간이 아닐 때 일반재고를 우선 감소시킬 수 있다.")
+    @DisplayName("프로모션 기간 아닐 때 재고관리 - 프로모션 혜택이 가능한 상품이지만 기간이 아닐 때 일반재고를 우선 감소시킬 수 있다. 또한 증정품의 개수와, 멤버십 할인을 올바르게 계산한다.")
     void shouldDecreaseNormalWhenNotPromotionPeriodAndHasPromotionBenefit() {
         assertNowTest(() -> {
             Order order = new Order("[오렌지주스-3]", store);
@@ -115,11 +121,13 @@ public class StoreTest {
             assertThat(promotion.getQuantity()).isEqualTo(8);
             assertThat(normal.getQuantity()).isEqualTo(0);
 
+            assertThat(receipt.getGiftProducts()).size().isEqualTo(0);
+            assertThat(receipt.getMembershipDiscount()).isEqualTo(1620);
         }, LocalDate.of(1999, 1, 1).atStartOfDay());
     }
 
     @Test
-    @DisplayName("프로모션 기간일 때 재고관리 - 1개 수량을 추가 결제할 때 프로모션 재고를 올바르게 감소시킬 수 있다.")
+    @DisplayName("프로모션 기간일 때 재고관리 - 1개 수량을 추가 결제할 때 프로모션 재고를 올바르게 감소시킬 수 있다. 또한 증정품의 개수와, 멤버십 할인을 올바르게 계산한다.")
     void shouldDecreasePromotionWhenPurchaseOneProduct() {
         Order order = new Order("[오렌지주스-1]", store);
         store.calculateWhenGetOneFreeCase(order, true);
@@ -132,9 +140,12 @@ public class StoreTest {
         assertThat(receipt.getTotalPurchaseCount()).isEqualTo(2);
         assertThat(promotion.getQuantity()).isEqualTo(7);
         assertThat(normal.getQuantity()).isEqualTo(2);
+
+        assertThat(receipt.getGiftProducts()).size().isEqualTo(1);
+        assertThat(receipt.getMembershipDiscount()).isEqualTo(0);
     }
 
-    @DisplayName("프로모션 기간일 때 재고관리 - 정가로 결제해야 하는 수량을 결제하지 않을 때 올바르게 재고를 감소시킬 수 있다.")
+    @DisplayName("프로모션 기간일 때 재고관리 - 정가로 결제해야 하는 수량을 결제하지 않을 때 올바르게 재고를 감소시킬 수 있다. 또한 증정품의 개수와, 멤버십 할인을 올바르게 계산한다.")
     @ParameterizedTest
     @CsvSource(value = {"[오렌지주스-9],8,1,2", "[오렌지주스-10],8,1,2"})
     void shouldDecreaseWhenNotPurchaseOriginalPrice(String name, int expectedTotalPurchase, int expectedPromotionQuantity, int expectedNormalPurchase) {
@@ -149,13 +160,19 @@ public class StoreTest {
         assertThat(receipt.getTotalPurchaseCount()).isEqualTo(expectedTotalPurchase);
         assertThat(promotion.getQuantity()).isEqualTo(expectedPromotionQuantity);
         assertThat(normal.getQuantity()).isEqualTo(expectedNormalPurchase);
+
+        int count = 0;
+        for (GiftProduct giftProduct : receipt.getGiftProducts()) {
+            count += giftProduct.getQuantity();
+        }
+        assertThat(count).isEqualTo(4);
+        assertThat(receipt.getMembershipDiscount()).isEqualTo(0);
     }
 
-
-    @DisplayName("프로모션 기간일 때 재고관리 - 정가로 결제해야 하는 수량을 결제할 때 올바르게 재고를 감소시킬 수 있다.")
+    @DisplayName("프로모션 기간일 때 재고관리 - 정가로 결제해야 하는 수량을 결제할 때 올바르게 재고를 감소시킬 수 있다. 또한 증정품의 개수와, 멤버십 할인을 올바르게 계산한다.")
     @ParameterizedTest
-    @CsvSource(value = {"[오렌지주스-9],9,0,2", "[오렌지주스-10],10,0,1"})
-    void shouldDecreaseWhenPurchaseOriginalPrice(String name, int expectedTotalPurchase, int expectedPromotionQuantity, int expectedNormalPurchase) {
+    @CsvSource(value = {"[오렌지주스-9],9,0,2,4,0", "[오렌지주스-10],10,0,1,4,0", "[컵라면-1],1,0,9,0,510"})
+    void shouldDecreaseWhenPurchaseOriginalPrice(String name, int expectedTotalPurchase, int expectedPromotionQuantity, int expectedNormalPurchase, int expectedGift, int expectedDiscount) {
         Order order = new Order(name, store);
         store.calculateWhenBuyOriginalPrice(order, true);
         Receipt receipt = store.getReceipt(true);
@@ -167,10 +184,17 @@ public class StoreTest {
         assertThat(receipt.getTotalPurchaseCount()).isEqualTo(expectedTotalPurchase);
         assertThat(promotion.getQuantity()).isEqualTo(expectedPromotionQuantity);
         assertThat(normal.getQuantity()).isEqualTo(expectedNormalPurchase);
+
+        int count = 0;
+        for (GiftProduct giftProduct : receipt.getGiftProducts()) {
+            count += giftProduct.getQuantity();
+        }
+        assertThat(count).isEqualTo(expectedGift);
+        assertThat(receipt.getMembershipDiscount()).isEqualTo(expectedDiscount);
     }
 
     @Test
-    @DisplayName("프로모션 기간일 때 재고관리 - 추가로 물어볼 필요가 없는 주문일 때 재고를 올바르게 감소시킬 수 있다.")
+    @DisplayName("프로모션 기간일 때 재고관리 - 추가로 물어볼 필요가 없는 주문일 때 재고를 올바르게 감소시킬 수 있다. 또한 증정품의 개수와, 멤버십 할인을 올바르게 계산한다.")
     void shouldDecreaseWhenNothingToAsk() {
         Order order = new Order("[오렌지주스-2]", store);
         store.calculateWhenNothingToAsk(order);
@@ -183,5 +207,12 @@ public class StoreTest {
         assertThat(receipt.getTotalPurchaseCount()).isEqualTo(2);
         assertThat(promotion.getQuantity()).isEqualTo(7);
         assertThat(normal.getQuantity()).isEqualTo(2);
+
+        int count = 0;
+        for (GiftProduct giftProduct : receipt.getGiftProducts()) {
+            count += giftProduct.getQuantity();
+        }
+        assertThat(count).isEqualTo(1);
+        assertThat(receipt.getMembershipDiscount()).isEqualTo(0);
     }
 }
